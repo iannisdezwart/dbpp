aa1c53d2f90337fe311fbda7eba52a25a4698557
iannisdezwart <iannis__@hotmail.com>
2022-04-21 22:06:44 +0200

    🚀 Add Into Memory BNL Join Algorithms

--- src/table-algorithms.hpp
+++ src/table-algorithms.hpp
@@ -22,8 +22,13 @@ OnDiskTable<OutRecord>
 bnl_join_into_disk(
 	const OnDiskTable<InRecordA> &a,
 	const OnDiskTable<InRecordB> &b,
-	std::function<bool(const InRecordA &, const InRecordB &)> filter,
-	std::function<OutRecord(const InRecordA &, const InRecordB &)> map)
+	std::function<bool(const InRecordA &, const InRecordB &)> filter =
+		OutRecord::is_match,
+	std::function<OutRecord(const InRecordA &, const InRecordB &)> map =
+		[](const InRecordA &a, const InRecordB &b)
+		{
+			return OutRecord(a, b);
+		})
 {
 	// Create a temporary output table.
 
@@ -64,8 +69,13 @@ OnDiskTable<OutRecord>
 bnl_join_into_disk(
 	const InMemoryTable<InRecordA> &a,
 	const InMemoryTable<InRecordB> &b,
-	std::function<bool(const InRecordA &, const InRecordB &)> filter,
-	std::function<OutRecord(const InRecordA &, const InRecordB &)> map)
+	std::function<bool(const InRecordA &, const InRecordB &)> filter =
+		OutRecord::is_match,
+	std::function<OutRecord(const InRecordA &, const InRecordB &)> map =
+		[](const InRecordA &a, const InRecordB &b)
+		{
+			return OutRecord(a, b);
+		})
 {
 	// Create a temporary output table.
 
@@ -107,8 +117,13 @@ OnDiskTable<OutRecord>
 bnl_join_into_disk(
 	const InMemoryTable<InRecordA> &a,
 	const OnDiskTable<InRecordB> &b,
-	std::function<bool(const InRecordA &, const InRecordB &)> filter,
-	std::function<OutRecord(const InRecordA &, const InRecordB &)> map)
+	std::function<bool(const InRecordA &, const InRecordB &)> filter =
+		OutRecord::is_match,
+	std::function<OutRecord(const InRecordA &, const InRecordB &)> map =
+		[](const InRecordA &a, const InRecordB &b)
+		{
+			return OutRecord(a, b);
+		})
 {
 	// Create a temporary output table.
 
@@ -150,12 +165,207 @@ OnDiskTable<OutRecord>
 bnl_join_into_disk(
 	const OnDiskTable<InRecordA> &a,
 	const InMemoryTable<InRecordB> &b,
-	std::function<bool(const InRecordA &, const InRecordB &)> filter,
-	std::function<OutRecord(const InRecordA &, const InRecordB &)> map)
+	std::function<bool(const InRecordA &, const InRecordB &)> filter =
+		OutRecord::is_match,
+	std::function<OutRecord(const InRecordA &, const InRecordB &)> map =
+		[](const InRecordA &a, const InRecordB &b)
+		{
+			return OutRecord(a, b);
+		})
 {
 	// Create a temporary output table.
 
 	OnDiskTable<OutRecord> out = OnDiskTable<OutRecord>::create_temp();
+
+	// Iterate over all rows in the on-disk table in blocks.
+
+	for (const InRecordA &row_a : a)
+	{
+		// Iterate over all rows in the in-memory table in blocks.
+
+		for (const InRecordB &row_b : b)
+		{
+			// If the join filter function returns true,
+			// add the joined row to the output table.
+
+			if (filter(row_a, row_b))
+			{
+				out.insert(map(row_a, row_b));
+			}
+		}
+	}
+
+	return out;
+}
+
+/**
+ * Joins two on-disk tables into memory using a given join predicate.
+ * Uses the block nested loop join algorithm.
+ *
+ * @param a The first table.
+ * @param b The second table.
+ * @param filter The join filter function.
+ * @param map The mapping function.
+ */
+template <typename OutRecord, typename InRecordA, typename InRecordB>
+InMemoryTable<OutRecord>
+bnl_join_into_memory(
+	const OnDiskTable<InRecordA> &a,
+	const OnDiskTable<InRecordB> &b,
+	std::function<bool(const InRecordA &, const InRecordB &)> filter =
+		OutRecord::is_match,
+	std::function<OutRecord(const InRecordA &, const InRecordB &)> map =
+		[](const InRecordA &a, const InRecordB &b)
+		{
+			return OutRecord(a, b);
+		})
+{
+	// Create an output table.
+
+	InMemoryTable<OutRecord> out;
+
+	// Iterate over all rows in the first table in blocks.
+
+	for (const InRecordA &row_a : a)
+	{
+		// Iterate over all rows in the second table in blocks.
+
+		for (const InRecordB &row_b : b)
+		{
+			// If the join filter function returns true,
+			// add the joined row to the output table.
+
+			if (filter(row_a, row_b))
+			{
+				out.insert(map(row_a, row_b));
+			}
+		}
+	}
+
+	return out;
+}
+
+/**
+ * Joins two in-memory tables into memory using a given join predicate.
+ * Uses the block nested loop join algorithm.
+ *
+ * @param a The first table.
+ * @param b The second table.
+ * @param filter The join filter function.
+ * @param map The mapping function.
+ */
+template <typename OutRecord, typename InRecordA, typename InRecordB>
+InMemoryTable<OutRecord>
+bnl_join_into_memory(
+	const InMemoryTable<InRecordA> &a,
+	const InMemoryTable<InRecordB> &b,
+	std::function<bool(const InRecordA &, const InRecordB &)> filter =
+		OutRecord::is_match,
+	std::function<OutRecord(const InRecordA &, const InRecordB &)> map =
+		[](const InRecordA &a, const InRecordB &b)
+		{
+			return OutRecord(a, b);
+		})
+{
+	// Create an output table.
+
+	InMemoryTable<OutRecord> out;
+
+	// Iterate over all rows in the first table in blocks.
+
+	for (const InRecordA &row_a : a)
+	{
+		// Iterate over all rows in the second table in blocks.
+
+		for (const InRecordB &row_b : b)
+		{
+			// If the join filter function returns true,
+			// add the joined row to the output table.
+
+			if (filter(row_a, row_b))
+			{
+				out.insert(map(row_a, row_b));
+			}
+		}
+	}
+
+	return out;
+}
+
+/**
+ * Joins an in-memory table with an on-disk table into memory using a
+ * given join predicate.
+ * Uses the block nested loop join algorithm.
+ *
+ * @param a The in-memory table.
+ * @param b The on-disk table.
+ * @param filter The join filter function.
+ * @param map The mapping function.
+ */
+template <typename OutRecord, typename InRecordA, typename InRecordB>
+InMemoryTable<OutRecord>
+bnl_join_into_memory(
+	const InMemoryTable<InRecordA> &a,
+	const OnDiskTable<InRecordB> &b,
+	std::function<bool(const InRecordA &, const InRecordB &)> filter =
+		OutRecord::is_match,
+	std::function<OutRecord(const InRecordA &, const InRecordB &)> map =
+		[](const InRecordA &a, const InRecordB &b)
+		{
+			return OutRecord(a, b);
+		})
+{
+	// Create an output table.
+
+	InMemoryTable<OutRecord> out;
+
+	// Iterate over all rows in the on-disk table in blocks.
+
+	for (const InRecordB &row_b : b)
+	{
+		// Iterate over all rows in the in-memory table in blocks.
+
+		for (const InRecordA &row_a : a)
+		{
+			// If the join filter function returns true,
+			// add the joined row to the output table.
+
+			if (filter(row_a, row_b))
+			{
+				out.insert(map(row_a, row_b));
+			}
+		}
+	}
+
+	return out;
+}
+
+/**
+ * Joins an on-disk table with an in-memory table into memory using a
+ * given join predicate.
+ * Uses the block nested loop join algorithm.
+ *
+ * @param a The on-disk table.
+ * @param b The in-memory table.
+ * @param filter The join filter function.
+ * @param map The mapping function.
+ */
+template <typename OutRecord, typename InRecordA, typename InRecordB>
+InMemoryTable<OutRecord>
+bnl_join_into_disk(
+	const OnDiskTable<InRecordA> &a,
+	const InMemoryTable<InRecordB> &b,
+	std::function<bool(const InRecordA &, const InRecordB &)> filter =
+		OutRecord::is_match,
+	std::function<OutRecord(const InRecordA &, const InRecordB &)> map =
+		[](const InRecordA &a, const InRecordB &b)
+		{
+			return OutRecord(a, b);
+		})
+{
+	// Create an output table.
+
+	InMemoryTable<OutRecord> out;
 
 	// Iterate over all rows in the on-disk table in blocks.
 
